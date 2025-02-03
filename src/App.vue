1b9eaf7b3c401063701f0d1a99bb327e2673f6c4
gerald.ofori@hubtel.com <geraldofori@Geralds-MacBook-Pro.local>
2025-02-03 20:54:13 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignUp />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from "./components/SignUp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SignUp,
+  },
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <img class="logo" src="../assets//app-logo.webp" alt="" />
+    <h1>Sign Up</h1>
+    <div class="register">
+      <input type="text" placeholder="Enter name" />
+      <input type="email" placeholder="Enter email" />
+      <input type="password" placeholder="Enter password" />
+      <button>Sign up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+.logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+  border-radius: 8px;
+}
+.register button {
+  width: 250px;
+  height: 35px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  cursor: pointer;
+  color: white;
+}
+</style>
